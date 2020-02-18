986ebf081ee0c44246a9170bd774f9e2a9d0d3d4
Glitch (hello-webpage) <none>
2020-02-18 21:18:39 +0000

    💝⛵️ Checkpoint ./index.html:376270/364 ./style.css:376270/859 ./app.js:376270/545 ./grammar.js:376270/6989

---
+++ grammar.js
@@ -0,0 +1,335 @@
+var grammar = {
+	"origin": [
+		"#contamination#. #source#. #culprit#. #eyewitness#. #exposure#. #response#."
+	],
+	
+	"firstSyl" : ["Ark", "Global", "Hammer", "Cent", "Dyna", "Archi", "Chem", "Petro"],
+    "lastSyl" : ["ex", "chem", "dyne", "eum", "tech", "ax", "zoom", "plex", "base", "com", "plus", "ent", "edge"],
+	
+	"corporationname": ["#firstSyl##lastSyl#"],
+
+	"contamination" : ["#time# #amount# #disaster#"],
+	
+	"source" : ["#mysterysource# appears to be #accident#"],
+	
+	"culprit" : ["The #manufacturer# #corporationname# #companytype# #operates# nearby #facilities#"],
+	
+	"time": [
+		"Yesterday",
+		"This morning",
+		"Today",
+		"Last night",
+		"Early this morning"
+	],
+	"amount": [
+		"an unspecified amount of",
+		"an unknown quantity of",
+		"5,000 gallons of",
+		"10,000 gallons of",
+		"15,000 gallons of",
+		"20,000 gallons of",
+		"25,000 gallons of"
+	],
+	"disaster": ["#air#",
+	"#water#",
+	"#gas#"],
+	
+	"gas": ["#poison# contaminated #area#"],
+	
+	"air": [
+		"#poisongas# contaminated #area#"
+	],
+
+"area": ["the surrounding area",
+"nearby towns",
+"#city#"],
+
+"city": ["Flint, Michigan",
+"New York City",
+"Los Angeles",
+"Chicago",
+"Houston, Texas",
+"Philadelphia",
+"Phoenix, Arizona",
+"Charlotte, North Carolina",
+"Long Beach, California",
+"Kansas City, Missouri",
+"Atlanta, Georgia",
+"Oakland, California"],
+
+"water": ["#poison# contaminated #waterbody#"
+],
+
+"poison": ["lead",
+"acetone cyanohydrin",
+"aniline",
+"benzenearsonic acid",
+"cadmium oxide",
+"carbon disulfide",
+"chlorophacinone",
+"diethyl chlorophosphate",
+"dithiobiuret",
+"ergocalciferol",
+"ethylenediamine",
+"formaldehyde cyanohydrin",
+"hydrogen selenide",
+"isopropylmethylpyrazolyl dimethylcarbamate",
+"mercuric oxide",
+"methanesulfonyl fluoride",
+"methyltrichlorosilane",
+"n-nitrosodimethylamine",
+"perchloromethylmercaptan",
+"phosphonothioic acid",
+"potassium cyanide",
+"sodium pentachlorophenate",
+"thiosemicarbazide",
+"xylylene dichloride",
+"4-methylcyclohexane methanol"],
+
+"waterbody": ["the Flint River",
+"Arkansas River",
+"Bear Creek",
+"Beaver Brook Creek",
+"Beaver Creek",
+"Bimini Waterway",
+"Biscayne Bay",
+"Bluff Creek",
+"Bowman Creek",
+"Budd Inlet",
+"Calibogue Sound",
+"Cape Fear River",
+"Catfish Lake",
+"Cherry Creek",
+"Darby Creek",
+"Devil's Lake",
+"Elk River",
+"Fly Creek",
+"Gastineau Channel",
+"Harbor Cove",
+"Harlem River",
+"Hot Springs Creek",
+"International Waters",
+"Kachemak Bay",
+"Lake Union",
+"Little Creek Cove",
+"Mahoning River",
+"Mystic River",
+"Old Hickory Lake",
+"Palmetto Bay",
+"Quartermaster Harbor",
+"Roanoke River",
+"San Marcos River",
+"Seven Seas Lagoon",
+"Tennessee River",
+"Tiger Point Creek",
+"Turtle Bay",
+"Vermilion Bay",
+"Whites Creek",
+"Gin Creek",
+"Muddy Creek Bay"],
+
+	"poisongas": [
+		"arsenic pentafluoride",
+		"boron trifluoride",
+		"chlorine trifluoride",
+		"hydrogen cyanide",
+		"phosphorus pentafluoride",
+		"tungsten hexafluoride"
+	],
+	"mysterysource": [
+		"The source",
+		"The point of origin",
+		"Ground zero",
+		"The contamination vector"
+	],
+	"accident": [
+		"a train derailment",
+		"a water treatment center",
+		"a bridge collapse",
+		"a jackknifed tractor trailer",
+		"a tanker truck accident",
+		"a power plant blackout",
+		"a building collapse",
+		"a mine explosion",
+		"a boiler explosion",
+		"an airplane crash",
+		"a faulty pipeline"
+	],
+	"manufacturer": [
+		"manufacturer",
+		"company",
+		"utility company",
+		"privately-owned",
+		"publicly-traded"
+	],
+	"companytype": [
+		"Group",
+		"Industries",
+		"Operations",
+		"Mines, Inc.",
+		"Resources LLP",
+		"Electric",
+		"Chemical Co.",
+		"Works"
+	],
+	"operates": [
+		"operates",
+		"manages",
+		"owns"
+	],
+	"facilities": [
+		"facilities",
+		"warehouses",
+		"factories",
+		"distribution centers",
+		"refineries"
+	],
+	"eyewitness": [
+		"#voice# #event#"
+	],
+	"voice": [
+		"Eyewitnesses report",
+		"Onlookers describe",
+		"Victims describe",
+		"First responders report",
+		"Nearby residents report"
+	],
+	"event": [
+		"a feathery plume",
+		"a shapeless growing thing",
+		"a dark black breathing thing",
+		"a black billowing cloud",
+		"a toxic event",
+		"a cloud of deadly chemicals",
+		"a chemical cloud",
+		"a chemical haze",
+		"an enormous floating dark mass",
+		"the smell of #odor# in the air"
+	],
+	"odor": [
+		"licorice",
+		"rotten eggs",
+		"burnt almonds",
+		"sulphur",
+		"sewage"
+	],
+	"exposure": [
+		"#duration# exposure may include #symptoms# and #symptoms#"
+	],
+	"duration": [
+		"Prolonged",
+		"Short-term",
+		"Brief",
+		"Long-term",
+		"Even momentary"
+	],
+	"symptoms": [
+		"lead poisoning",
+		"the growth of urgent lumps",
+		"skin irritation",
+		"sweaty palms",
+		"nausea",
+		"vomiting",
+		"shortness of breath",
+		"heart palpitations",
+		"a sense of déjà vu",
+		"convulsions",
+		"miscarriage",
+		"short or long-term coma",
+		"chemical burns",
+		"diarrhea",
+		"seizures",
+		"a burning sensation in the upper respiratory system",
+		"increased tear production",
+		"sudden death"
+	],
+	"response": [
+		"#government# #orders#",
+		"#corporate# #PR#",
+		"#media# #coverage#",
+		"President Trump tweeted about #tweets#"
+	],
+	"government": [
+		"The President",
+		"The Lieutenant Governor",
+		"The Secretary of Homeland Security",
+		"The mayor",
+		"The Governor",
+		"The EPA",
+		"The Attorney General",
+		"FEMA",
+		"The Department of Environmental Quality"
+	],
+	"tweets": [
+		"birth certificates",
+		"building a wall",
+		"his polling numbers",
+		"fake news"
+	],
+	"orders": [
+		"has insisted the water is safe to drink",
+		"declared a state of emergency",
+		"warned residents to drink bottled water",
+		"advised residents not to drink the water",
+		"ordered residents to shelter in place",
+		"issued evacuation orders",
+		"closed part of the interstate",
+		"ordered residents to abandon all domiciles",
+		"advised citizens to stay indoors",
+		"urges calm",
+		"recommends boiling drinking water",
+		"said there is no need to panic",
+		"said that although there's no evidence the water's not safe to drink, they also can't say it is safe"
+	],
+	"corporate": [
+		"The company",
+		"The local utility company",
+		"The water plant",
+		"The district water manager",
+		"The water and sewer department"
+	],
+	"PR": [
+		"has saved millions of dollars",
+		"acknowledges some concern",
+		"is not concerned",
+		"suggests residents take sensible precautions",
+		"blames the incident on #error#",
+		"has said an investigation is underway",
+		"assures residents that an investigation is underway, pending funding"
+	],
+	"error": [
+		"a one-time system failure",
+		"human error",
+		"faulty wiring",
+		"bad pipes",
+		"the EPA"
+	],
+	"media": [
+		"NPR",
+		"FOX News",
+		"MSNBC",
+		"CNN",
+		"Twitter"
+	],
+	"coverage": [
+		"reports that clean-up crews are on the scene",
+		"reports that #number#  #people# are in custody",
+		"reports that first responders #responders#",
+		"reports that hazmat crews have gone missing"
+	],
+	"number": [
+		"two",
+		"three",
+		"four"
+	],
+	"people": [
+		"people",
+		"possible terrorists",
+		"suspects",
+		"senior executives"
+	],
+	"responders": [
+		"are on the scene",
+		"have succumbed to fatal exposure"
+	]
+}
