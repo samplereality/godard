ec852d9e4f0a1b6a94804da9583f57af4c00fbda
Glitch (dont-drink-the-water) <none>
2021-10-28 01:21:56 +0000

    🐜📷 Checkpoint ./grammar.js:376270/43

--- grammar.js
+++ grammar.js
@@ -4,9 +4,9 @@ var source = {
   "girl" : ["The girl and her robot #activity#."],
   "happy" : ["danced", "played cards", "skipped rope", "told each other stories"],
   "sad" : ["watched the rain", "searched for home"],
-  "robot" : ["The robot thought #thoughts#"],
+  "robot" : ["The robot thought #thoughts#."],
   "thoughts" : ["#happyrobot#", "#sadrobot#"],
   "happyrobot" : ["they were in love"],
   "sadrobot" : ["his battery was dying"],
-  "news" : [""]
+  "news" : ["Days passed and the world grew warmer."]
 }
