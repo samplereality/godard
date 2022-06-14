edc2866bb2d7b01d8d0d017bfd13c3a5ff507718
Glitch (dont-drink-the-water) <none>
2022-06-14 18:36:32 +0000

    🛩🍀 Checkpoint ./grammar.js:376270/33

--- grammar.js
+++ grammar.js
@@ -1,7 +1,7 @@
 var rules = {
   start: ["$context $story"],
   context: ["Another day in the future. | The same things keep happening. | What happens next? | Gather 'round, children"],
-  story: ["$girl[2] | $robot[3] | $news $news[5]"],
+  story: ["$girl[2] | $robot[3] | $news $news"],
   activity: ["$happy | $sad"],
   girl: ["The girl and her robot $activity."],
   happy: [
@@ -26,12 +26,12 @@ var rules = {
 
 // Tips for modifying RiTa grammars:
 // Add these to the end of a rule or a phrase
-// .art() 	adds the correct article according to the phonemes 	art()
-// .cap() 	capitalizes the first letter of the phrase 	cap()
-// .uc() 	capitalizes all letters of the phrase 	uc()
-// .qq() 	adds (smart) quotes around the phrase 	qq()
-// .nr() 	ensures a choice will never repeat its output 	nr()
-// .s() 	pluralizes the noun phrase via the RiTa inflector 	s()
+// .art() 	adds the correct article according to the phonemes 	
+// .cap() 	capitalizes the first letter of the phrase 	
+// .uc() 	capitalizes all letters of the phrase 	
+// .qq() 	adds (smart) quotes around the phrase 
+// .nr() 	ensures a choice will never repeat its output 	
+// .s() 	pluralizes the noun phrase via the RiTa inflector
 //
 // More details at https://observablehq.com/@dhowe/tut-rita-grammars
 // and https://observablehq.com/@dhowe/riscript
