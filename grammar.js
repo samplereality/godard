62faf0d8f721422f095ef4ee5b1c2048ab8f2c24
Glitch (dont-drink-the-water) <none>
2022-06-14 02:50:41 +0000

    🐺🍨 Checkpoint ./grammar.js:376270/128

--- grammar.js
+++ grammar.js
@@ -2,11 +2,12 @@ var rules = {
   start: ["$girl | $robot | $news"],
   activity: ["$happy | $sad"],
   girl: ["The girl and her robot $activity."],
-  happy: ["danced", "played cards", "skipped rope", "told each other stories"],
+  happy: ["danced", "played cards", "skipped rope", "told each other stories", "talked about $topic. There was so much to say about $topic"],
   sad: ["watched the rain", "searched for home"],
   robot: ["The robot thought $thoughts."],
   thoughts: ["$happyrobot | $sadrobot"],
   happyrobot: ["they were in love"],
   sadrobot: ["his battery was dying"],
   news: ["Days passed and the world grew warmer."],
+  $topic: [".randomNoun()"]
 };
