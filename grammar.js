b60ebebd3bedf8d9343fe6b919f18ef887c5e29a
Glitch (dont-drink-the-water) <none>
2023-06-01 20:41:56 +0000

    🚿🐌 Checkpoint ./style.css:376270/170 ./index.html:376270/86 ./main.js:376270/452 ./grammar.js:376270/3230

--- grammar.js
+++ grammar.js
@@ -1,16 +1,12 @@
 var rules = {
   start: ["$context $story"],
-  context: ["Another day in the future. | The same things keep happening. | What happens next? | Gather 'round, children"],
-  story: ["$girl[2] | $robot[3] | $news $news"],
+  context: [
+    "Another day in the future. | The same things keep happening. | What happens next? | Gather 'round, children, and listen. | A new day dawned.",
+  ],
+  story: ["$girl | $robot | $news"],
   activity: ["$happy | $sad"],
   girl: ["The girl and her robot $activity."],
-  happy: [
-    "danced",
-    "played cards",
-    "skipped rope",
-    "told each other stories",
-    "talked about the $topic. There was so much to say about the $topic",
-  ],
+  happy: ["danced", "played cards", "skipped rope", "told each other stories"],
   sad: ["watched the (clouds | rain)", "searched for home"],
   robot: ["The robot thought $thoughts."],
   thoughts: ["$happyrobot | $sadrobot"],
@@ -19,19 +15,18 @@ var rules = {
   ],
   sadrobot: ["his battery was (dying | running low | empty)"],
   news: [
-    "Days passed and the world grew warmer. | The nights were short and hot. | The (troops | rebels).nr() were (losing | gaining) ground. | (Another | A).nr() (rocket | satellite | ship).nr() launched at (dawn | daybreak).",
+    "Days passed and the world grew warmer. | The nights were short and hot. | The (troops | rebels) were (losing | gaining) ground. | (Another | A) (rocket | satellite | ship) launched at (dawn | daybreak).",
   ],
-  $topic: [".randomNoun()"],
 };
 
 // Tips for modifying RiTa grammars:
 // Add these to the end of a rule or a phrase
-// .art() 	adds the correct article according to the phonemes 	
-// .cap() 	capitalizes the first letter of the phrase 	
-// .uc() 	capitalizes all letters of the phrase 	
-// .qq() 	adds (smart) quotes around the phrase 
-// .nr() 	ensures a choice will never repeat its output 	
-// .s() 	pluralizes the noun phrase via the RiTa inflector 	
+// .art() 	adds the correct article according to the phonemes
+// .cap() 	capitalizes the first letter of the phrase
+// .uc() 	capitalizes all letters of the phrase
+// .qq() 	adds (smart) quotes around the phrase
+// .nr() 	ensures a choice will never repeat its output
+// .s() 	pluralizes the noun phrase via the RiTa inflector
 //
 // More details at https://observablehq.com/@dhowe/tut-rita-grammars
 // and https://observablehq.com/@dhowe/riscript
--- main.js
+++ main.js
@@ -1,22 +1,7 @@
 // Define some functions that can be performed from within a grammar
 
-let context = {
-  randomNoun: () => RiTa.randomWord({ pos: "nn" }),
-  pluralNoun: () => RiTa.randomWord({ post: "nns" }),
-  randomAdjective: () => RiTa.randomWord({ pos: "jj" }),
-  randomAdverb: () => RiTa.randomWord({ pos: "rb" }),
-  
-};
-
 function generatePrompt() {
-  let rg = RiTa.grammar(rules, context); // load our grammar
+  let rg = RiTa.grammar(rules); // load our grammar
   let result = rg.expand();
   $("#output").text(result);
 }
-
-var addRhyme = function(word) { 
-  let res = RiTa.rhymes(word);    // get the rhymes
-  return word + ' rhymes with ' + RiTa.random(res); // append a random one
-}
-
-RiTa.addTransform('rhymes', addRhyme);
