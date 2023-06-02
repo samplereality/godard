8fd8b02f0de0ddbab1162300251a6f3e7bc7f789
Glitch (dont-drink-the-water) <none>
2023-06-02 17:17:02 +0000

    🏊🏼👾 Checkpoint ./main.js:376270/415 ./grammar.js:376270/4598 ./index.html:376270/47

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 // Define some functions that can be performed from within a grammar
 
+let context = {
+  randomNoun: () => RiTa.randomWord({ pos: "nn" }),
+};
+
 function generatePrompt() {
-  let rg = RiTa.grammar(rules); // load our grammar
+  let rg = RiTa.grammar(rules, context); // load our grammar
   let result = rg.expand();
-  $("#output").text(result);
+  $("#output").html(result);
 }
