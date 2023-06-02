7b3f0d4fa99016cdabd32867989a32b4787bac43
Glitch (dont-drink-the-water) <none>
2023-06-02 15:54:38 +0000

    🐋🐧 Checkpoint ./grammar.js:376270/5980

--- grammar.js
+++ grammar.js
@@ -1,29 +1,191 @@
 var rules = {
-  start: "$context $story",
-  context: 
-    "Another day in the future. | The same things keep happening. | What happens next? | Gather 'round, children, and listen. | A new day dawned.",
-  story: "$girl | $robot | $news",
-  activity: "$happy | $sad",
-  girl: "The girl and her robot $activity.",
-  happy: "danced | played cards | skipped rope | told each other stories",
-  sad: "watched the (clouds | rain) | searched for home",
-  robot: "The robot thought $thoughts.",
-  thoughts: "$happyrobot | $sadrobot",
-  happyrobot: 
-    "they were in love | the world seemed brighter (today | this evening | tonight)",
-  sadrobot: "his battery was (dying | running low | empty)",
-  news: "Days passed and the world grew warmer. | The nights were short and hot. | The (troops | rebels) were (losing | gaining) ground. | (Another | A) (rocket | satellite | ship) launched at (dawn | daybreak)."
-};
+  start: "He $arrives and then—$bang—$refinery explodes. $Americans $love $Cannibals armed with $bazookas $fire $railroad She $falls $Speed to $Vincennes $cohn $train and $speaks $men $kill He reads $Mao $Monte throws $bomb at $Diderot He $goes He $peddles $ending",
 
-// Tips for modifying RiTa grammars:
-// Add these to the end of a rule or a phrase
-// .art() 	adds the correct article according to the phonemes
-// .cap() 	capitalizes the first letter of the phrase
-// .uc() 	capitalizes all letters of the phrase
-// .qq() 	adds (smart) quotes around the phrase
-// .nr() 	ensures a choice will never repeat its output
-// .s() 	pluralizes the noun phrase via the RiTa inflector
-//
-// More details at https://observablehq.com/@dhowe/tut-rita-grammars?collection=%40dhowe%2Frita
-//
-// and https://observablehq.com/@dhowe/riscript?collection=%40dhowe%2Frita
+  "arrives": [
+    "arrives",
+    "is already there reading the sayings of Mao",
+    "lies dead on the superhighway with brains spattered",
+    "is killing himself",
+    "harangues a crowd",
+    "runs along the street",
+    "jumps out of a window"
+  ],
+
+  "bang": "bang | splash | splat | wham | rat-tat-tat", "mumble mumble"],
+
+  "refinery": [
+    "a refinery",
+    "a kindergarten",
+    "Notre Dame",
+    "the Communist Party headquarters",
+    "the House of Parliament",
+    "the Parthenon",
+    "the office of Le Figaro",
+    "the Elysée",
+    "Paris"
+  ],
+
+  "Americans": [
+    "The Americans",
+    "The Germans",
+    "French paratroopers",
+    "The Vietnamese",
+    "Arabs",
+    "Israelis",
+    "The police"
+  ],
+
+  "love": ["make love.", "do not make love."],
+
+  "Cannibals": [
+    "Cannibals",
+    "Indians",
+    "Hordes of accountants",
+    "Dissident Commmunists",
+    "Crazed truck drivers"
+  ],
+
+  "bazookas": [
+    "bazookas",
+    "yagatans",
+    "copies of Le Figaro",
+    "pirate's sabers",
+    "submachine guns",
+    "cans of $color paint",
+    "Picasso paintings",
+    "little red books",
+    "picture postcards"
+  ],
+
+  "fire": [
+    "fire",
+    "throw rocks",
+    "throw bombs",
+    "throw empty cans of #color# paint",
+    "pour some slippery stuff"
+  ],
+
+  "color": ["red", "green", "blue", "yellow", "black"],
+
+  "railroad": [
+    "on the railroad.",
+    "on the Elysée.",
+    "on the University of Nanterre.",
+    "in Piazza Navona.",
+    "all over the road."
+  ],
+
+  "falls": [
+    "falls, $bullets",
+    "is thrown out of the window by CIA agents.",
+    "is captured by paratroopers.",
+    "is killed by Australian aborigines."
+  ],
+
+  "bullets": [
+    "riddled with bullets from a $rifle",
+    "with a gaping wound in the belly.",
+    "spewing forth streams of $color paint.",
+    "making love with Voltaire."
+  ],
+
+  "rifle": ["rifle.", "Loquat."],
+
+  "Speed": [
+    "At mad speed",
+    "Unsteadily",
+    "Very, very slowly",
+    "Remaining still while the background (a process shot) moves"
+  ],
+
+  "Vincennes": [
+    "Vincennes,",
+    "Nanterre,",
+    "Flins,",
+    "Place de la Bastille,",
+    "Clignancourt,",
+    "Venice,"
+  ],
+
+  "cohn": [
+    "Cohn-Bendit",
+    "Jacques Servan-Schreiber",
+    "Jean-Paul Sartre",
+    "Pier Paolo Pasolini",
+    "D'Alembert"
+  ],
+
+  "train": [
+    "catches the train",
+    "misses the train",
+    "goes on a bicycle",
+    "roller skates"
+  ],
+
+  "speaks": ["speaks.", "bursts into tears.", "shouts Viva Guevara!"],
+
+  "men": ["Two men", "A band of Indians"],
+
+  "kill": ["kill her.", "kill everybody.", "kill nobody."],
+
+  "Mao": [
+    "sayings of Mao.",
+    "quotations from Brecht.",
+    "the Declaration of the Rights of Man.",
+    "Saint-John Perse.",
+    "Prince Korzybski.",
+    "Eluard.",
+    "Lo Sun.",
+    "Charles Péguy.",
+    "Rosa Luxemburg."
+  ],
+
+  "Monte": [
+    "Montesquieu",
+    "Diderot",
+    "Sade",
+    "Restif de la Bretonne",
+    "Pompidou"
+  ],
+
+  "bomb": ["a bomb", "a tomato", "$color paint"],
+
+  "Diderot": [
+    "Diderot.",
+    "Daniel Cohn-Bendit.",
+    "Nixon.",
+    "Madame de Sevigné.",
+    "Voiture.",
+    "Van Vogt.",
+    "Einstein."
+  ],
+
+  "goes": [
+    "kills himself.",
+    "goes away.",
+    "kills all the others.",
+    "throws a bomb at the Arc de Triomphe.",
+    "blows up an electronic brain.",
+    "empties onto the ground various cans of $color paint."
+  ],
+
+  "peddles": [
+    "peddles Le Figaro.",
+    "peddles the saying of Mao.",
+    "writes a ta-tze-bao.",
+    "reads verses of Pierre Emmanuel.",
+    "watches a Chaplin movie."
+  ],
+
+  "ending": [
+    "The natives arrive.",
+    "The paratroopers arrive.",
+    "The Germans arrive.",
+    "Hordes of starving accountants brandishing sabers arrive.",
+    "Armored cars arrive.",
+    "Pier Paolo Pasolini with Pompidou arrives.",
+    "The bank holiday traffic arrives.",
+    "Diderot, selling the Encyclopédie door-to-door, arrives.",
+    "The Marxist-Leninist Union on skateboards arrive."
+  ]
+}
